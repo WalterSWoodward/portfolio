175f0486edb25fae38ade90152845383217bbfcd
walterswoodward <walter.sekope.woodward@gmail.com>
2021-02-15 09:22:57 -0800

    Removes basename attr from <Router> which appears to be unnecessary

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 
 import Home from "./components/Home.js";
 
 class App extends Component {
   render() {
+    // removed `basename={process.env.PUBLIC_URL}` attr from <Router>
+    // per reactrouter's docs, this is only necessary if the application is served
+    // from a subdirectory
     return (
-      <Router basename={process.env.PUBLIC_URL}>
-        <Switch>
-          <Route path="/" component={Home} exact />
-        </Switch>
+      <Router>
+        <div>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/dashboard">Dashboard</Link>
+            </li>
+          </ul>
+
+          <hr />
+          <Switch>
+            <Route path="/" component={Home} exact />
+          </Switch>
+        </div>
       </Router>
     );
   }
