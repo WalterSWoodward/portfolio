c88dbf18ae397a589e30316b9ae86e3f87f4768d
walterswoodward <walter.sekope.woodward@gmail.com>
2021-02-15 14:44:05 -0800

    Use Fragment module

--- src/components/About.js
+++ src/components/About.js
@@ -9,33 +9,34 @@ class About extends React.PureComponent {
 
   renderLanding = function (){
     return (
-   
-      <div
-        id='fade_wrapper'
-        className="about__wrap"
-        style={{
-          background: this.props.linearGradient
-        }}
-      >
-        <div id='box1_text'>
-          <div className='box1_name'>WALTER WOODWARD</div>
-          <div className='box1_description'>FULL STACK WEB DEVELOPER</div>
-          <a
-            className='box1_link2'
-            href='#portfolio'
-            aria-label="Click here to view Walter's current projects"
-          >
-            PORTFOLIO
-          </a>
-          <a
-            className='box1_link3'
-            href='#contact'
-            aria-label="Click here to view Walter's Contact form"
-          >
-            CONTACT
-          </a>
+      <Fragment>
+        <div
+          id='fade_wrapper'
+          className="about__wrap"
+          style={{
+            background: this.props.linearGradient
+          }}
+        >
+          <div id='box1_text'>
+            <div className='box1_name'>WALTER WOODWARD</div>
+            <div className='box1_description'>FULL STACK WEB DEVELOPER</div>
+            <a
+              className='box1_link2'
+              href='#portfolio'
+              aria-label="Click here to view Walter's current projects"
+            >
+              PORTFOLIO
+            </a>
+            <a
+              className='box1_link3'
+              href='#contact'
+              aria-label="Click here to view Walter's Contact form"
+            >
+              CONTACT
+            </a>
+          </div>
         </div>
-      </div>
+      </Fragment>
     );
   }
 
