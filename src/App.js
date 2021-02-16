dfa36339b728c70b3dffea56501555ed3f7d0153
walterswoodward <walter.sekope.woodward@gmail.com>
2021-02-15 18:32:34 -0800

    Removes Home module and some comments

--- src/App.js
+++ src/App.js
@@ -5,32 +5,17 @@ import {
   Switch
 } from "react-router-dom";
 
-import Home from "./components/Home.js";
 import About from "./components/About.js";
 import Contact from "./components/Contact.js";
 import Portfolio from "./components/Portfolio.js";
 
 class App extends Component {
   render() {
-    // removed `basename={process.env.PUBLIC_URL}` attr from <Router>
-    // per reactrouter's docs, this is only necessary if the application is served
-    // from a subdirectory
     return (
       <Router>
         <Fragment>
-          {/* <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/dashboard">Dashboard</Link>
-            </li>
-          </ul> */}
           <Switch>
-            <Route path="/" component={Home} exact />
+            <Route path="/" component={About} exact />
             <Route path="/about" component={About} />
             <Route path="/contact" component={Contact} />
             <Route path="/portfolio" component={Portfolio} />
